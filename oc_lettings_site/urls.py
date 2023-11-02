1280d9a3c3e83eeff052df169460dc2a4c6ebf13
Alex Brs <alexbrise.40@gmail.com>
2023-11-02 03:42:43 +0100

    Modular archi on works

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from profiles.views import profile, profiles_index
-from lettings.views import letting, lettings_index
+from profiles import views as pviews
+from lettings import views as lviews
 from .views import index
 
 urlpatterns = [
     path("", index, name="index"),
-    path("lettings/", lettings_index, name="lettings_index"),
-    path("lettings/<int:letting_id>/", letting, name="letting"),
-    path("profiles/", profiles_index, name="profiles_index"),
-    path("profiles/<str:username>/", profile, name="profile"),
+    path("lettings/", lviews.index, name="lettings_index"),
+    path("lettings/<int:letting_id>/", lviews.letting, name="letting"),
+    path("profiles/", pviews.index, name="profiles_index"),
+    path("profiles/<str:username>/", pviews.profile, name="profile"),
     path("admin/", admin.site.urls),
 ]
