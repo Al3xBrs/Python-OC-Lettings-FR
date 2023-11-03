404e8b35d39d0d361aec1ce161f283d73a4c723a
Alex Brs <alexbrise.40@gmail.com>
2023-11-03 21:03:48 +0100

    minors update

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
 from profiles.models import Profile
-from django.contrib.auth.models import User
 
 
 class CreateTestCase(TestCase):
     def test_create_profile(self):
-        profile_test = Profile.objects.get(favorite_city="TEST")
-        self.assertEqual(profile_test.favorite_city, "TEST")
+        profile_test = Profile.objects.get(user_id=5)
+        self.assertEqual(profile_test.user_id, 5)
