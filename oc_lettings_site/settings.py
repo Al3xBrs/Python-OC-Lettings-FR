32b908092d04d5aeb26621d9b3c13f0ac40385a2
Alex Brs <alexbrise.40@gmail.com>
2023-11-09 13:36:06 +0100

    render

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -73,11 +73,10 @@ WSGI_APPLICATION = "oc_lettings_site.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default="postgresql://postgres:postgres@localhost:5432/oc_lettings_site",
-        conn_max_age=600,
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "oc-lettings-site.sqlite3"),
+    }
 }
 
 
