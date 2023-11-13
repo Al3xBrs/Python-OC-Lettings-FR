a42feef858e72a116def63ba34fe6aaf1c21eda7
Alex Brs <alexbrise.40@gmail.com>
2023-11-13 15:23:38 +0100

    update

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -115,7 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
@@ -124,7 +123,7 @@ STATICFILES_DIRS = [
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Sentry config
 
 DSN = os.environ.get("DSN")
