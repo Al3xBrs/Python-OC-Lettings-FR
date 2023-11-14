55cb14e60ef44d2e81b3a388c4c22ce3d4fb64bc
Alex Brs <alexbrise.40@gmail.com>
2023-11-14 02:16:48 +0100

    update host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = f"{os.environ.get('SECRET_KEY')}"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
