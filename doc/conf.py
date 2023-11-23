66fcefcdf2337cd99c519cb432b0414116428679
Alex Brs <alexbrise.40@gmail.com>
2023-11-23 13:39:32 +0100

    a commit

--- doc/conf.py
+++ doc/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Al3xBrs-OC-Lettings'
-copyright = '2023, Al3xBrs'
-author = 'Al3xBrs'
-release = '11/21/2023'
+project = "Al3xBrs-OC-Lettings"
+copyright = "2023, Al3xBrs"
+author = "Al3xBrs"
+release = "11/21/2023"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
