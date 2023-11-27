c987c03db18b52a3b6c4d4f4aee53c1833fd4cf8
Alex Brs <alexbrise.40@gmail.com>
2023-11-27 11:03:47 +0100

    a commit

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = f"{config.get('SECRET_KEY')}"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
